1e9e424fb5d90161bd896fbf7ae7b54132b4ae9c
Suraj Pai <bspai@bwh.harvard.edu>
2024-12-19 12:25:06 -0500

    Update LUNA modelling and DLCS feat extract

--- evaluation/dlcs_feature_extractor.py
+++ evaluation/dlcs_feature_extractor.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 from base_feature_extractor import extract_all_features, save_features
 
 def get_split_data(split):
@@ -10,7 +11,11 @@ def preprocess_row(row):
     """Preprocess a row from DLCS dataset"""
     row = row.copy()
     row["image_path"] = f'/mnt/data1/datasets/DukeLungNoduleDataset/{row["ct_nifti_file"]}'
-    return row
+
+    if os.path.exists(row["image_path"]):
+        return row
+    else:
+        return None
 
 def extract_features():
     """Extract features for DLCS dataset"""
